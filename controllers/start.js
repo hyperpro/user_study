e726543f2d56017794b542f9900cbc32725af0ad
Eric Roger Shang <sheric@linux1.cs.uchicago.edu>
2019-07-09 16:54:19 -0500

    changed git ignore

--- controllers/start.js
+++ controllers/start.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 
 const vid_folder = "buffered2";
 var vid_path = "videos/" + vid_folder;
-var video_url = "https://github.com/sheric98/QoEProject/raw/master/videos/" + vid_folder;
+var video_url = "https://github.com/sheric98/QoEProject/raw/master/videos/" + vid_folder + "/";
 
 var num_vids;
 fs.readdir(vid_path, function(err, files) {
