79c29bd9da54d75a8d9309ca10645bdba4b56ce3
Eric Roger Shang <sheric@linux2.cs.uchicago.edu>
2019-11-11 22:02:46 -0600

    change to buffer_location3

--- controllers/start.js
+++ controllers/start.js
@@ -2,7 +2,7 @@
 var getOder = require('../models/random');
 var fs = require('fs');
 
-const vid_folder = "buffer_location2";
+const vid_folder = "buffer_location3";
 var vid_path = "videos/" + vid_folder;
 var video_url = "https://github.com/sheric98/QoEProject/raw/master/videos/" + vid_folder + "/";
 
