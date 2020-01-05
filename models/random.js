859a48ec11e4509116ca4ec527b09d72beb172c5
hyperpro <zhangbjb@126.com>
2020-01-05 15:31:53 -0600

    xz

--- models/random.js
+++ models/random.js
@@ -1,19 +1,36 @@
+// function getOrder(start, end) {
+//     var length = end - start;
+//     var myorder = new Array();
+//     var index = 0;
+//     while (index < length+1) {
+//         var flag = true;
+//         var num = index
+//         for (var i in myorder) {
+//             if (myorder[i] == num) {
+//                 flag = false;
+//             }
+//         }
+//         if (flag == true) {
+//             myorder[index] = num;
+//             index++;
+//         }
+//     }
+//     for(var i in myorder) {
+//         myorder[i] = myorder[i] + 1;
+//     }
+//     return myorder;
+// }
+
+
 function getOrder(start, end) {
     var length = end - start;
     var myorder = new Array();
     var index = 0;
     while (index < length+1) {
-        var flag = true;
-        var num = parseInt(Math.random() * (length + 1));
-        for (var i in myorder) {
-            if (myorder[i] == num) {
-                flag = false;
-            }
-        }
-        if (flag == true) {
-            myorder[index] = num;
-            index++;
-        }
+        
+        var num = index
+        myorder[index] = num;
+        index++;
     }
     for(var i in myorder) {
         myorder[i] = myorder[i] + 1;
