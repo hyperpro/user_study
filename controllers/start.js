3901fd6c87d9527a7fa2b2a41d4bd73defdf8fea
xz <you@example.com>
2020-02-17 09:01:51 -0800

    xz

--- controllers/start.js
+++ controllers/start.js
@@ -2,7 +2,7 @@
 var getOder = require('../models/random');
 var fs = require('fs');
 
-const vid_folder = "buffer_location3";
+const vid_folder = "video_importance";
 var vid_path = "videos/" + vid_folder;
 var video_url = "https://github.com/hyperpro/user_study/raw/master/videos/" + vid_folder + "/";
 
